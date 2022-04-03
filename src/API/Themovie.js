204b7d24163ffaf62f8150ddb2a1635f0dd0ef48
carlosalesilva <Carlinhos@2509>
2022-04-02 23:53:09 -0300

    criando a lista de filmes

--- src/API/Themovie.js
+++ src/API/Themovie.js
@@ -7,8 +7,8 @@ const API_BASE = 'https://api.themoviedb.org/3';
 const basicFetch = async (endpoint) => {
     const req = await fetch(`${API_BASE}${endpoint}`);
     const json = await req.json();
-    return json();
-}
+    return json; 
+};
 
 export default{
     getHomeList: async() => {
@@ -26,22 +26,22 @@ export default{
             {
                 slug:'comedy',
                 title:'Comédia',
-                 items: await basicFetch(`/discover/movie?with_genres=35?language=pt-BR&api_key=${API_KEY}`)
+                 items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug:'action',
                 title:'Ação',
-                items: await basicFetch(`/discover/movie?with_genres=28?language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug:'horror',
-                title:'terror',
-                 items: await basicFetch(`/discover/movie?with_genres=27?language=pt-BR&api_key=${API_KEY}`)
+                title:'Terror',
+                 items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug:'romance',
                 title:'Romance',
-                items: await basicFetch(`/discover/movie?with_genres=10749?language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
             },
         ]; 
     }
--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React,{ useEffect, useState } from 'react'; 
+import Themovie from './API/Themovie';
+import MovieList from './components/movielist/MovieList';
+import Navbar from './components/navbar/Navbar';
+import './App.css';
 
-export default function App() {
+
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+  //Quando a tela for carregada vai executar a função 
+  useEffect(()=>{
+    const loadAll = async () => {
+      //pegando a lista total 
+      let list = await Themovie.getHomeList();
+      setMovieList(list); 
+    }
+
+    loadAll();
+  }, []);
+
   return (
-    <div className="app">
-     
-
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key)=>(
+           <MovieList key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
     </div>
   );
 }
